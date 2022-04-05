e693bd17a76344f6fa14fe21cda08d7e3bf672f2
unknown <you@example.com>
2022-04-05 12:22:19 +0700

    first commit

---
+++ 61136364_intent/app/src/main/java/com/example/a61136364_intent/activity_home.java
@@ -0,0 +1,21 @@
+package com.example.a61136364_intent;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class activity_home extends AppCompatActivity {
+    TextView showdata;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        showdata = findViewById(R.id.tvUserName);
+        showdata.setText(getIntent().getStringExtra("data"));
+    }
+}
